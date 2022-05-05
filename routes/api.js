9f4834005c4adf0c5ce1c035368abbc8ed5675a9
Simon Levey <lev_47@hotmail.com>
2022-05-06 02:31:27 +0800

    save and delete work

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,22 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const fs = require('fs');
+const { v4 } = require('uuid');
 
 const dbPath = path.join(__dirname, '..', 'db', 'db.json');
 
+
+/**
+ * 
+ * @returns {Array}
+ */
 function getNotes(){
     return JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
 
+}
+function saveNotesToDb(notes){
+
+    fs.writeFileSync(dbPath, JSON.stringify(notes), 'utf8');
 }
 
 
@@ -22,11 +32,45 @@ router.post('/api/notes', (req, res) => {
     //create a new note
     console.log(req.body); 
     //read the request body for the note title and text
-    const title = req.body.title;
-    const text = req.body.text;
-    //generate an ID to the new note
+    //const title = req.body.title;
+    //const text = req.body.text;
+    const {title, text} = req.body;
 
+    //generate an ID to the new note
+    const newNote = {
+        id: v4(),
+        //title: title,
+        title,  //same as above
+        text: text,
+    }
+    
     //save the new note to the end of the existing note array
+    const notes = getNotes();
+
+    notes.push(newNote);
+
+    saveNotesToDb(notes);
+
+    res.json({
+        data: 'ok',
+    })
+});
+
+router.delete('/api/notes/:id', (req, res) => {
+
+    //get all the notes
+    const notes = getNotes();
+    //filter out the target note
+    const result = notes.filter((note) => {
+        return note.id !== req.params.id
+    });
+    //resave to db
+    saveNotesToDb(result);
+
+    res.json({
+        data: 'ok',
+    })
+
 })
 
 
