75c498df6e6bf6acc3921ee7707bd4b2de01eccb
Simon Levey <lev_47@hotmail.com>
2022-05-05 23:11:39 +0800

    added nodemon

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+const path = require('path');
 const express = require('express');
 
 const app = express();
@@ -8,6 +8,16 @@ const PORT = 3001;
 
 
 app.use( express.static('public') );
+
+
+app.get('/', (req, res) => {
+
+
+    const indexHtmlPath = path.join(__dirname, 'public', 'index.html');
+
+    res.sendFile(indexHtmlPath)
+
+})
 
 app.listen(PORT, function(){
     console.log(`App is running on http://localhost:${PORT}`);
