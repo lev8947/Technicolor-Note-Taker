8dc0587c4eafc81605581c865875ed6172f59e81
Simon Levey <lev_47@hotmail.com>
2022-05-05 23:00:27 +0800

    server.js PORT added

---
+++ server.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+
+const app = express();
+
+
+const PORT = 3001;
+
+
+app.use( express.static('public') );
+
+app.listen(PORT, function(){
+    console.log(`App is running on http://localhost:${PORT}`);
+});
+
+
